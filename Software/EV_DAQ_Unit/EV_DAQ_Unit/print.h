01ee0bb624f03f6223205aeb3669569e5d545592
jfritz022 <jfritz022@gmail.com>
2016-09-15 21:34:14 -0500

    Added Print module
    This module will contain the STDIO routines for selectable printing over
    the LCD or UART

---
+++ Software/EV_DAQ_Unit/EV_DAQ_Unit/print.h
@@ -0,0 +1,15 @@
+/** @file print.h
+* @brief Print routines for LCD and UART over stdio
+*
+* @par
+* COPYRIGHT NOTICE: (c) 2016 John Fritz
+* All rights reserved
+*/
+
+#ifndef print_h__
+#define print_h__
+
+int8_t max8(int8_t num1, int8_t num2);
+
+
+#endif // print_h__
